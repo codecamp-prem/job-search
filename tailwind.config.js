01ae4f4c64d06de7b19d1e0a9d8624cab85b1868
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-06-05 22:18:59 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
