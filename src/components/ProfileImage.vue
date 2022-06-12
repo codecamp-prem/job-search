33f75177f21c6bb31ab59a1e3f22c7e6860bab8a
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-06-12 18:55:48 +0545

    Vue derective if-else, on. Adding methods to vue object

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="defaultProfileImage" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+import defaultProfileImage from "@/assets/images/zelda.png";
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      defaultProfileImage,
+    };
+  },
+};
+</script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,38 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Jobs Search NP");
   });
+  it("Display Menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life in Nepal",
+      "How we Hire?",
+      "Students",
+      "Jobs",
+    ]);
+  });
+  describe("When user is logged out", () => {
+    it("prompt user to sign in", () => {
+      const wrapper = mount(MainNav);
+      const loginButton = wrapper.find("[data-test='login-button']");
+      expect(loginButton.exists()).toBe(true);
+    });
+  });
+  describe("When user is logged in", () => {
+    it("show user profile image", async () => {
+      const wrapper = mount(MainNav);
+      let profileImage = wrapper.find("[data-test='profile-image']");
+      expect(profileImage.exists()).toBe(false);
+
+      const loginButton = wrapper.find("[data-test='login-button']");
+      await loginButton.trigger("click");
+      profileImage = wrapper.find("[data-test='profile-image']");
+      expect(profileImage.exists()).toBe(true);
+    });
+  });
 });
